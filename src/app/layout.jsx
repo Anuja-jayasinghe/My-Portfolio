29a9ec35fbb267472bb4bf811f15a8fed1b76766
Anuja Jayasinghe <anujajayasinhe@gmail.com>
2025-05-15 20:51:56 +0530

    preview-metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,6 +20,20 @@ export const metadata = {
     icon: '/favicon.svg',
   },
   description: "Welcome to my personal portfolio website.",
+  openGraph: {
+    images: [
+      {
+        url: '/website-preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'Website Preview',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/website-preview.png'],
+  },
 };
 
 export default function RootLayout({ children }) {
